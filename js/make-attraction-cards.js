7a0e6a4d034ef38e736d49d36cbf4d5b057784ec
ricky-bruner <alteredecho07@yahoo.com>
2018-08-05 23:42:30 -0500

    made js page for card build module

---
+++ js/make-attraction-cards.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let buildAttractions = require("./attraction-cards.js");
+
+function makeAttractionCards(attractions, areaType, attractionType){
+    let attractionDiv = document.querySelector(".attraction-content");
+    attractionDiv.innerHTML = "";
+    console.log(attractionType, areaType);
+    console.log(typeof attractionType, typeof areaType);
+    let intAttractionType = parseInt(attractionType);
+    let intAreaType = parseInt(areaType);
+    console.log(intAttractionType, intAreaType);
+    console.log(typeof intAttractionType, typeof intAreaType);
+    let sortedArray = [];
+    attractions.forEach((item) => {
+        if(item.type_id === intAttractionType && item.area_id === intAreaType){
+                console.log(sortedArray, sortedArray.length);
+                sortedArray.push(item);
+                attractionDiv.innerHTML += 
+                buildAttractions(item.name, item.description);
+        }
+    });
+    if(sortedArray.length === 0){
+        attractionDiv.innerHTML +=
+        `<p>Please look for these attractions in other areas of NSS Amusement Arcade</p>`;
+    };   
+}
+
+module.exports = makeAttractionCards;
