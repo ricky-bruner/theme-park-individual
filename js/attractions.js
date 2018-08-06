8d7aa7c05f3a95d6443365cd7d8ba67463ddb727
ricky-bruner <alteredecho07@yahoo.com>
2018-08-05 23:40:36 -0500

    rebuild entire page so that each database only pulled from once upon refresh

--- js/attractions.js
+++ js/attractions.js
@@ -1,41 +1,27 @@
-// "use strict";
-
-// let db = require('./db-calls.js');
-// let makeAttractionCards = require('./attraction-build.js');
-
-// let attractions = {};
-
-// db.fetchAttractions()
-// .then((result) => {
-//     attractions = result;
-//     console.log("attractions:", attractions);
-//     makeAttractionCards(attractions);
-// });
-
-// module.exports = attractions;
 "use strict";
 
 let db = require('./db-calls.js');
-let buildFunctions = require('./attraction-build.js');
+let beginAttractions = require('./attraction-build.js');
 
 let areas;
 let types;
 let attractions;
 
-
 db.fetchAreas().then((result) => {
     areas = result;
+    console.log("areas db was hit");
     db.fetchTypes().then((result) => {
         types = result;
+        console.log("types db was hit");
         db.fetchAttractions().then((result) => {
             attractions = result;
+            console.log("attractions db was hit");
             console.log("attractions:", attractions);
             console.log("types:", types);
             console.log("areas:", areas);
-            buildFunctions.beginAttractions(areas, types, attractions);
+            beginAttractions(areas, types, attractions);
         });
     });
 });
-
 
 module.exports = attractions, areas, types;
