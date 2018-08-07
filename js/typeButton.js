b7bf7aa29367a25013cad20aa5ca6471cf804e9a
ricky-bruner <alteredecho07@yahoo.com>
2018-08-07 00:36:33 -0500

    reworked types section

--- js/typeButton.js
+++ js/typeButton.js
@@ -3,13 +3,9 @@
 function makeTypeArea(dataArray){
     let typeDiv = document.querySelector(".type-container");
     dataArray.forEach((item) => {
-    //document.createElement("button")
-
     let typeComponent = item.name.charAt(0).toUpperCase() + item.name.slice(1);
-
         typeDiv.innerHTML +=`<button id="type${item.id}">${typeComponent}</button>`;
     });
 }
-
 
 module.exports= makeTypeArea;
